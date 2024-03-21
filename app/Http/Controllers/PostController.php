14d06733e4e2677f4c7be39b15f3cc5ebfbf89f2
0kutan <okutanab@gmail.com>
2024-03-21 11:41:53 +0900

    記事投稿作成画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,6 +34,13 @@ public function edit(Post $post)
 {
     return view('posts/edit')->with(['post' => $post]);
 }
+
+public function update(PostRequest $request, Post $post)
+{
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+    return redirect('/posts/' . $post->id);
+}
 }
 
 
