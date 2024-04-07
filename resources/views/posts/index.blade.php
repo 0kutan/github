7c9f3bd5459a1221b2a9f8723c03c065dfdfc330
0kutan <okutanab@gmail.com>
2024-04-07 15:01:38 +0900

    paginatelimit追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    
-    </head>
-    <body class="antialiased">
+<x-app-layout>
+    <x-slot name="header">
+        　index
+    </x-slot>
+   
+
         <h1>Blog Name</h1>
         <a href="/posts/create">create</a>
             <div class='posts'>
@@ -33,5 +30,5 @@
                 }
             }
         </script>
-    </body>
+    </x-app-layout>
 </html>
