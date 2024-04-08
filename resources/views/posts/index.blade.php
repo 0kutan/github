0fde8adbb97187fc5a07a6d191d2591b00abed21
0kutan <okutanab@gmail.com>
2024-04-08 12:08:18 +0900

    web API

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,22 +4,19 @@
         　index
     </x-slot>
    
-
+   <body>
         <h1>Blog Name</h1>
         <a href="/posts/create">create</a>
             <div class='posts'>
-                @foreach($posts as $post)
-                <div class='post'>
-                <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
-                <p class='body'>{{ $post->body }}</p>
-                <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
-                @csrf
-                @method('DELETE')
-                <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
-                </form>
+                @foreach($questions as $question)
+            </div>
+            <a href="https://terarail.com/questions/{{ $question['id'] }}">
+            {{$question['title'] }}
+            </a>
             </div>
         @endforeach
         </div>
+    </body>    
         <div class='paginate'>{{ $posts->links()}}</div>
         <script>
             function deletePost(id) {
